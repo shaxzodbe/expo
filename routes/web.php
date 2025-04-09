e81c008bcab1249b1066f47530aa8e7a291248d2
Shaxzodbek <shsulaymanov@texnomart.uz>
2025-04-09 13:36:26 +0500

    Updated

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
 Route::get('/events/{event}', [MainController::class, 'show'])->name('event.details');
-Route::get('/admin/login', \Filament\Pages\Auth\Login::class);
-Route::post('/admin/login', \Filament\Http\Controllers\Auth\LogoutController::class);

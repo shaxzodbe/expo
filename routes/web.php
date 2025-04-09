ab8ff623d7aac00359a54f8689cc276a234daefe
Shaxzodbek <shsulaymanov@texnomart.uz>
2025-04-09 12:32:06 +0500

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainController;
-use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
 Route::get('/events/{event}', [MainController::class, 'show'])->name('event.details');
-Route::get('admin/login', function () {
-    return view('filament::auth.login');
-})->name('filament.auth.login');
-
-Route::post('admin/login', [Filament::class, 'handleLogin'])->name('filament.auth.login.post');
